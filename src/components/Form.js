54f65e30658c58ba1233468070b1e2882959242a
Eddahkarambu <gikunaeddah@gmail.com>
2021-09-07 12:39:29 +0300

    updated form.js file

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 
-const Form =({setInputText, todos,setTodos})=>{
+const Form =({setInputText, todos,setTodos,inputText})=>{
     const inputTextHandler = (e) =>{
       console.log(e.target.value);
       setInputText(e.target.value);
@@ -9,14 +9,15 @@ const Form =({setInputText, todos,setTodos})=>{
     const submitTodoHandler =(e) =>{
       e.preventDefault();
       setTodos([
-        ....todos,{text:inputTextHandler,completed:false,}
-      ])
-
+        ...todos,
+        {text:inputText,completed:false,id:Math.random() *1000},
+      ]);
+      setInputText("");
     };
     
     return(
         <form>
-        <input onChange={inputTextHandler} type="text" className="todo-input" />
+        <input value={inputText} onChange={inputTextHandler} type="text" className="todo-input" />
         <button onClick={submitTodoHandler} className="todo-button" type="submit">
           <i className="fas fa-plus-square"></i>
         </button>
